9fcff6ee9f2e47214684af9fd53a702bd7a24a3d
weirdkhar <rsh615@uowmail.edu.au>
2016-09-09 12:24:47 +1000

    Cleanup and comment
    Working through the functions to remove extraneous commented text and
    adding comments for some functions.

--- CPC_TSI.py
+++ CPC_TSI.py
@@ -35,20 +35,7 @@ def Load_to_HDF(RawDataPath,
     #Iterate through to load the raw files 
     for file in filelist:
         # Read cpc csv file
-        read_cpc_csv(file, output_h5_filename, output_file_frequency, InputTZ, OutputTZ)
-    ''' 
-        #Correct for Timezone offsets caused by AIM exporting process
-        if InputTZ-OutputTZ != 0 :
-            data = TimeZoneCorrection(data, CurrentTZ = InputTZ, OutputTZ = OutputTZ)
-        
-        # Read data file and determine whether to save the file in a new file, or append to existing file
-        save_to_hdf(data, output_h5_filename, output_file_frequency)
-        
-        # Delete temporary file
-        if os.path.isfile(file.split('.')[0]+'_temp.h5'):
-            os.remove(file.split('.')[0]+'_temp.h5')
-    '''
-    
+        read_cpc_csv(file, output_h5_filename, output_file_frequency, InputTZ, OutputTZ)  
     
     #Save the files that have already been loaded to file for next update
     with open('files_loaded.txt', 'wb') as f:
@@ -60,15 +47,6 @@ def Load_to_HDF(RawDataPath,
             filelist = filelist + files_already_loaded
         pickle.dump(filelist, f)
 
-    
-#    # Iterate through files
-#    periods.sort()
-#    for i in range(0, len(periods)):
-#        output_h5_filename_ = output_h5_filename + '_' + str(periods[i])
-#        filelist_ = list(filelist_df[filelist_df['id'] == periods[i]]['filenames'])
-
-#        save_cpc_to_hdf(RawDataPath, output_h5_filename, InputTZ, OutputTZ)
-    
     if resample_time:    
         resample_timebase(RawDataPath, output_h5_filename,variable = output_h5_filename,time_int=['5S'])
     
@@ -198,6 +176,10 @@ def read_cpc_csv(read_filename, output_filename_base, output_file_frequency, Inp
     return
     
 def read_cpc_csv_row_AIM9(read_filename, output_filename_base, output_file_frequency, InputTZ=0, OutputTZ=0):
+	# function to read row based cpc exported data from AIM versions 9 or below. 
+	# This is VERY slow due to the exported format from AIM 
+	#(error messages from sampling accumulate with each sample, causing the parsing of the data to slow down as it progresses).
+	
     import datetime
     
     if output_file_frequency == 'all':
@@ -234,50 +216,7 @@ def read_cpc_csv_row_AIM9(read_filename, output_filename_base, output_file_frequ
                 sample_timestamp = pd.to_datetime(rows.index[0][1]+' '+rows.index[0][2], format = '%m/%d/%y %H:%M:%S')
                 sample_length = int(rows.index[0][3])
                 samplenum = rows.index[0][0]
-            
-#'''            
-#            try:
-#                if type(rows.index) != pd.indexes.multi.MultiIndex:
-#                    # Extract initial timestamp
-#                    sample_timestamp = pd.to_datetime(rows.iloc[0,0]+' '+rows.iloc[0,1], format = '%m/%d/%y %H:%M:%S')
-#                elif len(rows.index[0]) >= 3:
-#                    sample_timestamp = pd.to_datetime(rows.index[0][1]+' '+rows.index[0][2], format = '%m/%d/%y %H:%M:%S')
-#                    
-#                # Extract sample length
-#                sample_length = int(rows.iloc[0,2])
-#                # Extract sample number
-#                samplenum = rows.index[0]
-#                
-#            except TypeError:
-#                # Extract initial timestamp
-#                sample_timestamp = pd.to_datetime(rows.index[0][1]+' '+rows.index[0][2], format = '%m/%d/%y %H:%M:%S')
-#                
-#                # Extract sample length                
-#                if len(rows.index[0]) == 3:
-#                    sample_length = int(rows.iloc[0,0])
-#                elif len(rows.index[0]) >= 4:
-#                    sample_length = int(rows.index[0][3])
-#                else:
-#                    print('help!')
-#
-#                # Extract sample number
-#                samplenum = rows.index[0][0]
-#            except ValueError:# Extract initial timestamp
-#                sample_timestamp = pd.to_datetime(rows.index[0][1]+' '+rows.index[0][2], format = '%m/%d/%y %H:%M:%S')
-#                
-#                # Extract sample length                
-#                if len(rows.index[0]) == 3:
-#                    sample_length = int(rows.iloc[0,0])
-#                elif len(rows.index[0]) >= 4:
-#                    sample_length = int(rows.index[0][3])
-#                else:
-#                    print('help!')
-#
-#                # Extract sample number
-#                samplenum = rows.index[0][0]
-#            except IndexError:
-#                print('')
-#'''       
+      
             # Make sample timestamp array
             timestamps = [sample_timestamp + datetime.timedelta(seconds=x) for x in range(0, sample_length)]
             # Remove all non-numeric data (e.g. errors)
@@ -302,17 +241,17 @@ def read_cpc_csv_row_AIM9(read_filename, output_filename_base, output_file_frequ
             print('Successfully saved sample ' + str(samplenum) + ' to file.')
             
             i += 1 #iterate
-            
-#            if samplenum > 273:
-#                print('')
-        
+      
         else:
             data_present=False       
 
     return
 
 def read_cpc_csv_column(read_filename, output_filename_base, output_file_frequency):
-    colnames = ['Timestamp','Concentration'] #Instantiate    
+    # function to read column based cpc exported data from AIM versions 9 or below. 
+	# This is slow - preferencially use the row based function 
+	
+	colnames = ['Timestamp','Concentration'] #Instantiate    
     
     # Load date
     date_row = pd.read_csv(read_filename,
@@ -358,24 +297,7 @@ def read_cpc_csv_column(read_filename, output_filename_base, output_file_frequen
         for i in range(0, len(data_temp)):
             if data_temp['Timestamp'][i] < data_temp['Timestamp'][0]:
                 data_temp['Timestamp'][i] = data_temp['Timestamp']+pd.Timedelta(days=1)
-        
-#        
-#        
-#        for k in range(0, k_end):
-#            time1 = pd.to_datetime(data_temp['Timestamp'][k])
-#            if type(time1) == pd.tslib.Timestamp: #avoid the end of a sample comment.                           
-#                if k > 0:
-#                    # Increment the date when it moves over midnight            
-#                    time0 = pd.to_datetime(data_temp['Timestamp'][k-1])
-#                    if type(time0) == pd.tslib.Timestamp:
-#                        tdiff = time1.hour-time0.hour 
-#                        if tdiff < 0:
-#                            date = date+pd.Timedelta(days=1)
-#                timestamp_str = str(date.date())+" "+str(time1.time())
-#                data_temp = data_temp.replace(
-#                    to_replace = data_temp['Timestamp'][k], 
-#                    value = pd.to_datetime(timestamp_str, format = "%Y-%m-%d %H:%M:%S")
-#                    )
+
         #Set index to Timestamp
         data_temp = data_temp.set_index('Timestamp') 
         
@@ -384,88 +306,10 @@ def read_cpc_csv_column(read_filename, output_filename_base, output_file_frequen
         print('Successfully saved sample ' + str(j) + ' to file.')
         
         del data_temp
-        '''
-        # Initialise data variable, otherwise append data
-        try:
-            data
-        except NameError:
-            data = data_temp[0:k_end] # data doesn't exist yet, initialise
-        else:
-            # data has been initialised, therefore, just append.
-            data = data.append(data_temp[0:k_end])
-        if os.path.exists(read_filename.split('.')[0]+'_'+str(j-1)+'of'+str(j_lim)+'.h5'):
-            os.remove(read_filename.split('.')[0]+'_'+str(j-1)+'of'+str(j_lim)+'.h5')
-        
-        data.to_hdf(read_filename.split('.')[0]+'_'+str(j)+'of'+str(j_lim)+'.h5', key=output_filename_base)
-        '''
+
     
     return #data
 
-def read_cpc_csv_OLD(read_filename, output_filename_base):
-    colnames = ['Timestamp','Concentration'] #Instantiate    
-    
-    # Load date
-    date_row = pd.read_csv(read_filename,
-                           nrows = 1,
-                           skiprows = range(0,4)
-                           )
-    
-    # Find the number of samples in the file
-    temp = pd.read_csv(read_filename,nrows = 1,skiprows = range(0,2))
-    j_lim = ''
-    k = 1
-    while isinstance(j_lim,str) & (k > 0):
-        if 'Unnamed' not in temp.columns[-k]:
-            j_lim = int(temp.columns[-k])
-        k = k+1
-
-    #Iterate through each sample            
-    for j in range(1, j_lim+1): 
-    
-        date_str = date_row.columns[2*j-1]
-        date = pd.to_datetime(date_str.split('.')[0])
-        
-        data_temp = pd.read_csv(read_filename, 
-                                names = colnames,
-                                skiprows = range(0,18), 
-                                engine='python',
-                                skipinitialspace = True, 
-                                usecols=range(2*j-2,2*j)
-                                )
-                            
-        k_end = data_temp[data_temp.Concentration.isnull()].index[0]
-        data_temp = data_temp[0:k_end]
-        
-        for k in range(0, k_end):
-            time1 = pd.to_datetime(data_temp['Timestamp'][k])
-            if type(time1) == pd.tslib.Timestamp: #avoid the end of a sample comment.                           
-                if k > 0:
-                    # Increment the date when it moves over midnight            
-                    time0 = pd.to_datetime(data_temp['Timestamp'][k-1])
-                    if type(time0) == pd.tslib.Timestamp:
-                        tdiff = time1.hour-time0.hour 
-                        if tdiff < 0:
-                            date = date+pd.Timedelta(days=1)
-                timestamp_str = str(date.date())+" "+str(time1.time())
-                data_temp = data_temp.replace(
-                    to_replace = data_temp['Timestamp'][k], 
-                    value = pd.to_datetime(timestamp_str, format = "%Y-%m-%d %H:%M:%S")
-                    )
-        #Set index to Timestamp
-        data_temp = data_temp.set_index('Timestamp') 
-        # Initialise data variable, otherwise append data
-        try:
-            data
-        except NameError:
-            data = data_temp[0:k_end] # data doesn't exist yet, initialise
-        else:
-            # data has been initialised, therefore, just append.
-            data = data.append(data_temp[0:k_end])
-        data.to_hdf(read_filename.split('.')[0]+'_'+str(j)+'of'+str(j_lim+1)+'.h5', key=output_filename_base)
-    
-    
-    return data
- 
 def TimeZoneCorrection(DataFrame, CurrentTZ, ConvertToUTC = True, OutputTZ = 0):
     import pandas as pd
     
@@ -529,33 +373,6 @@ def resample_timebase(data=0,RawDataPath='',input_h5_filename='',variable='CN',t
         data_resamp.to_hdf(outputfilename, key=variable)
     
     return data_resamp
-
-#def RVI_uwy_filter(uwy_merge_data,
-#               uwy_path='c:/Dropbox/RuhiFiles/Research/Projects/2016_V02_CAPRICORN/Data_Raw/Underway/',
-#               BC_lim = 0.2, 
-#               wind_dir_mask = False, windDir_lLim = 110, windDir_uLim = 250,
-#               radon_mask = False, 
-#               wind_sensor_disagreement_mask = False,
-#               SaveToFile = True):
-#
-#    import numpy as np
-#    import pandas as pd
-#    if 'mask' not in uwy_merge_data.columns:
-#        import RVI_Underway
-#        RVI_Underway.create_uwy_masks(uwy_path,
-#                                      apply_mask_to_create_filt_dataset=False,
-#                                      BC_lim = BC_lim, 
-#                                      wind_dir_mask = wind_dir_mask, 
-#                                      windDir_lLim = windDir_lLim, 
-#                                      windDir_uLim = windDir_uLim,
-#                                      radon_mask = radon_mask, 
-#                                      wind_sensor_disagreement_mask = wind_sensor_disagreement_mask,
-#                                      SaveToFile = SaveToFile)
-#        
-#    uwy_merge_data.loc[pd.isnull(uwy_merge_data['mask'])] = np.nan
-#
-#    return uwy_merge_data
-
 
 def flow_cal(data, measured_flows_df, set_flow_rate, polydeg=2):
     ''' Calibrates CPC_data for measured flow rates.
