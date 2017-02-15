c39e74b7c3b10444b63473ffeeb2307510cd79c2
Ruhi Humphries <ruhihumphries@gmail.com>
2017-02-15 15:04:36 +1100

    Major format cleanup

--- CCNC.py
+++ CCNC.py
@@ -5,14 +5,21 @@ version: 0.1
 date: 2016-08-31
 """
 import pandas as pd
+import pandas as pd
+import numpy as np
 import os
 import glob
 import pickle
 import atmoscripts
 
-def Load_to_HDF(DataPath, output_h5_filename = 'CCNC', resample_timebase = None, concat_file_frequency = 'all'):
-    #To reload data from the h5 file
-    
+def Load_to_HDF(DataPath, 
+				output_h5_filename = 'CCNC', 
+				resample_timebase = None, 
+				concat_file_frequency = 'all'
+				):
+    '''
+	To reload data from the h5 file
+    '''
     os.chdir(DataPath)
     
     filelist = glob.glob('*.csv')
@@ -47,14 +54,15 @@ def Load_to_HDF(DataPath, output_h5_filename = 'CCNC', resample_timebase = None,
     periods.sort()
     for i in range(0, len(periods)):
         output_h5_filename_ = output_h5_filename + '_' + str(periods[i])
-        filelist_ = list(filelist_df[filelist_df['id'] == periods[i]]['filenames'])
+        filelist_ = list(filelist_df[
+                                filelist_df['id'] == periods[i]]['filenames'])
 
         save_ccn_to_hdf(filelist_, output_h5_filename_, resample_timebase)
     
     return None
 
 def get_unique_periods(filelist, frequency):
-     # Extract all unique values of in the filelist (do this using the set command)    
+     # Extract all unique values in the filelist (do this using set )    
     months = set(get_month_label(filelist))  
     days = set(get_day_label(filelist))
 
@@ -75,8 +83,10 @@ def get_week_label(filelist):
     # Extract the week number of each dates in the filelist
     import datetime
     dates = [f[13:19] for f in filelist]
-    weeknum = [str(datetime.date(2000+int(day[0:2]),int(day[2:4]),int(day[4:6])).isocalendar()[1]) for day in dates]
-    year = [str(datetime.date(2000+int(day[0:2]),int(day[2:4]),int(day[4:6])).isocalendar()[0]) for day in dates]
+    weeknum = [str(datetime.date(2000+int(day[0:2]),
+               int(day[2:4]),int(day[4:6])).isocalendar()[1]) for day in dates]
+    year = [str(datetime.date(2000+int(day[0:2]),int(day[2:4]),
+               int(day[4:6])).isocalendar()[0]) for day in dates]
     #week_label = ['_wk' + str(s) for s in list(weeknum)]
     week_label = [x+'_wk'+y for x,y in zip(year,weeknum)]
     return week_label
@@ -92,9 +102,11 @@ def get_year_label(filelist):
 
 def save_ccn_to_hdf(filelist, output_h5_filename, resample_timebase = None):
     
-    if os.path.isfile(output_h5_filename +'.h5'): #If previous file exists, append, if not start new
+    #If previous file exists, append, if not start new
+    if os.path.isfile(output_h5_filename +'.h5'): 
         
-        files_already_loaded = atmoscripts.read_filelist_from_file('files_loaded.txt')
+        files_already_loaded = atmoscripts.read_filelist_from_file(
+                                                        'files_loaded.txt')
         #with open('files_loaded.txt', 'rb') as f:
         #    files_already_loaded = pickle.load(f)        
         
@@ -150,13 +162,14 @@ def read_ccn_csv(filelist):
     # Specify the column names once.
     colnames = [
                 'Time', 'Current SS', 'Temps Stabilized', 'Delta T', 'T1 Set', 
-                'T1 Read', 'T2 Set', 'T2 Read', 'T3 Set', 'T3 Read', 'Nafion Set', 
-                'T Nafion', 'Inlet Set', 'T Inlet', 'OPC Set', 'T OPC', 'T Sample', 
-                'Sample Flow', 'Sheath Flow', 'Sample Pressure', 'Laser Current', 
-                'overflow', 'Baseline Mon', '1st Stage Mon', 'Bin #', 'Bin 1', 
-                'Bin 2', 'Bin 3', 'Bin 4 ', 'Bin 5', 'Bin 6', 'Bin 7', 'Bin 8', 
-                'Bin 9', 'Bin 10', 'Bin 11', 'Bin 12', 'Bin 13', 'Bin 14', 'Bin 15',
-                'Bin 16', 'Bin 17', 'Bin 18', 'Bin 19', 'Bin 20', 'CCN Number Conc', 
+                'T1 Read', 'T2 Set', 'T2 Read', 'T3 Set', 'T3 Read', 
+                'Nafion Set', 'T Nafion', 'Inlet Set', 'T Inlet', 'OPC Set', 
+                'T OPC', 'T Sample', 'Sample Flow', 'Sheath Flow', 
+                'Sample Pressure', 'Laser Current', 'overflow', 'Baseline Mon',
+                '1st Stage Mon', 'Bin #', 'Bin 1', 'Bin 2', 'Bin 3', 'Bin 4 ', 
+                'Bin 5', 'Bin 6', 'Bin 7', 'Bin 8', 'Bin 9', 'Bin 10', 
+                'Bin 11', 'Bin 12', 'Bin 13', 'Bin 14', 'Bin 15', 'Bin 16', 
+                'Bin 17', 'Bin 18', 'Bin 19', 'Bin 20', 'CCN Number Conc', 
                 'Valve Set', 'Alarm Code', 'Alarm Sum'
                 ]
     delete_temp_files_manually = False
@@ -164,7 +177,8 @@ def read_ccn_csv(filelist):
     fname_previous = ''
                 
       
-    # If there are LOTS of files, break up into groups of 100 before combining into the final set (to manage RAM)
+    # If there are LOTS of files, break up into groups 
+    # before combining into the final set (to manage RAM)
     filelim = 50
     if len(filelist) > filelim:
         needs_final_grouping = True
@@ -192,7 +206,8 @@ def read_ccn_csv(filelist):
                             usecols=range(49)
                             )
         # Read date from csv file 
-        data['date'] = str(pd.read_csv(filelist[0], names = ['label', 'date'], skiprows = range(2,len(data)+6))['date'][1])                                 
+        data['date'] = str(pd.read_csv(filelist[0], names = ['label', 'date'], 
+                            skiprows = range(2,len(data)+6))['date'][1])                                 
         for i in range(i0, i1):
                 # Load csv data        
                 data_temp = pd.read_csv(filelist[i], 
@@ -203,13 +218,16 @@ def read_ccn_csv(filelist):
                             usecols=range(49)
                             )  
                 # Read date from csv file 
-                data_temp['date'] = str(pd.read_csv(filelist[i], names = ['label', 'date'], skiprows = range(2,len(data)+6))['date'][1])
+                data_temp['date'] = str(pd.read_csv(filelist[i], 
+                                        names = ['label', 'date'], skiprows = 
+                                        range(2,len(data)+6))['date'][1])
     
                 #Append new csv file data to existing dataframe
                 data = data.append(data_temp)#, ignore_index=True) 
                 
                 # Save new data to file
-                fname_current = 'CCNC_noIndex_temp_'+str(i0)+'to'+str(i+2)+'of'+str(len(filelist)+1)+'.h5'
+                fname_current = 'CCNC_noIndex_temp_'+str(i0)+'to'+str(i+2)+\
+                                'of'+str(len(filelist)+1)+'.h5'
                 data.to_hdf(fname_current, key='CCN')
                 
                 # Remove the temporary file    
@@ -222,7 +240,8 @@ def read_ccn_csv(filelist):
                 fname_previous = fname_current
         
         # Create timstamp from date and time columns
-        data['timestamp'] = pd.to_datetime(data['date']+' '+data['Time'], format = "%m/%d/%y %H:%M:%S")
+        data['timestamp'] = pd.to_datetime(data['date']+' '+data['Time'], 
+                                            format = "%m/%d/%y %H:%M:%S")
         
         
         # Drop any duplicates which may be there, based only on the Timestamp
@@ -247,7 +266,8 @@ def read_ccn_csv(filelist):
         del data, data_temp
         
         for j in range(0, j1):
-            data_temp = pd.read_hdf('CCN_group_'+str(j+1)+'of'+str(j1)+'.h5', key='CCN')
+            data_temp = pd.read_hdf('CCN_group_'+str(j+1)+'of'+str(j1)+'.h5', 
+                                    key='CCN')
             try:
                 data
             except NameError:
@@ -258,7 +278,7 @@ def read_ccn_csv(filelist):
             if os.path.isfile('CCN_group_'+str(j+1)+'of'+str(j1)+'.h5'):
                 os.remove('CCN_group_'+str(j+1)+'of'+str(j1)+'.h5')
     
-    # Clean up any files that couldn't be deleted due to being locked previously
+    # Clean up any files that couldnt be deleted due to being locked previously
     if delete_temp_files_manually:
         for k in range(0,len(temp_files_to_del)):
             os.remove(temp_files_to_del[k])
@@ -266,26 +286,33 @@ def read_ccn_csv(filelist):
     return data, fname_current
     
     
-def resample_timebase(data=0,RawDataPath='',input_h5_filename='',variable='CCN',time_int='default'):
+def resample_timebase(
+                      data=0,
+                      RawDataPath='',
+                      input_h5_filename='',
+                      variable='CCN',
+                      time_int='default'
+                      ):
     ### Time resampling
 
-    import pandas as pd
-    import numpy as np
-    import os
     
-    if not isinstance(data, pd.DataFrame): #if no data provided, try to load from file
+    
+    #if no data provided, try to load from file
+    if not isinstance(data, pd.DataFrame):
         if (not RawDataPath == '') & (not input_h5_filename == ''):
             os.chdir(RawDataPath)
             if os.path.isfile(input_h5_filename+'.h5'): 
                 data = pd.read_hdf(input_h5_filename+'.h5', key=variable)
         else:
-            print("Please input either a dataframe or a datapath and filename where data can be found")
+            print("Please input either a dataframe or \
+                  a datapath and filename where data can be found")
             return
     
     
     # define time resampling intervals unless specified in function input
     if time_int == 'default':
-        time_int = ['5S','1Min', '5Min', '10Min', '30Min', '1H', '3H', '6H', '12H', '1D']    
+        time_int = ['5S','1Min', '5Min', '10Min', \
+                    '30Min', '1H', '3H', '6H', '12H', '1D']    
     
     
     # define MAD calculation
@@ -299,13 +326,19 @@ def resample_timebase(data=0,RawDataPath='',input_h5_filename='',variable='CCN',
         t_int = time_int[i]    
         # Initialise    
         data_resamp = sub.resample(t_int,fill_method=None).median()
-        data_resamp['ccn_count'] = sub_ccn.resample(t_int,fill_method=None).count()
-        data_resamp['ccn_med'] = sub_ccn.resample(t_int,fill_method=None).median()
-        data_resamp['ccn_mad'] = sub_ccn.resample(t_int,fill_method=None).apply(mad)
-        data_resamp['ccn_avg'] = sub_ccn.resample(t_int,fill_method=None).mean()
-        data_resamp['ccn_std'] = sub_ccn.resample(t_int,fill_method=None).std()
+        data_resamp['ccn_count'] = sub_ccn.resample(t_int,
+                                                    fill_method=None).count()
+        data_resamp['ccn_med'] = sub_ccn.resample(t_int,
+                                                  fill_method=None).median()
+        data_resamp['ccn_mad'] = sub_ccn.resample(t_int,
+                                                  fill_method=None).apply(mad)
+        data_resamp['ccn_avg'] = sub_ccn.resample(t_int,
+                                                  fill_method=None).mean()
+        data_resamp['ccn_std'] = sub_ccn.resample(t_int,
+                                                  fill_method=None).std()
         
-        # Rename the cloud droplet bins so they make sense when the full data is merged
+        # Rename the cloud droplet bins so they make sense when the 
+        # full data is merged
         data_resamp.rename(columns={'Bin 1': 'CDN Bin 1',
                                     'Bin 2': 'CDN Bin 2',
                                     'Bin 3': 'CDN Bin 3',
@@ -368,16 +401,19 @@ def DataQC(CCN_data,
 
     
     ### Filter primary dataset
-    with np.errstate(invalid = 'ignore'): # Ignore error warnings caused by arithmetic on nans
+    with np.errstate(invalid = 'ignore'): # Ignore error warnings caused by 
+                                          # arithmetic on nans
     
-        # Alarms detected by software - note many of these only activate after a certain time period
+        # Alarms detected by software - note many of these only activate 
+        # after a certain time period in the CCNC software
         CCNC_data.loc[CCNC_data['Alarm Code'] > 0] = np.nan
     
         # Concentration lower than 10 /cm3 (as per factory setting)
         CCNC_data.loc[CCNC_data['CCN Number Conc'] < 10] = np.nan
         
         # Flow ratio outside 10 +/- 0.4
-        CCNC_data['Flow Ratio'] = CCNC_data['Sheath Flow'] / CCNC_data['Sample Flow']
+        CCNC_data['Flow Ratio'] = \
+                        CCNC_data['Sheath Flow'] / CCNC_data['Sample Flow']
         CCNC_data.loc[CCNC_data['Flow Ratio'] > (FlowRatio + 1)]= np.nan
         CCNC_data.loc[CCNC_data['Flow Ratio'] < (FlowRatio - 1)]= np.nan
         
@@ -392,23 +428,29 @@ def DataQC(CCN_data,
         CCNC_data.loc[CCNC_data['Laser Current'] > 120] = np.nan    
         
         # Temperatures deviating from their setpoints       
-        CCNC_data.loc[abs(CCNC_data['T1 Set'] - CCNC_data['T1 Read']) > T1diffLim] = np.nan
-        CCNC_data.loc[abs(CCNC_data['T2 Set'] - CCNC_data['T2 Read']) > T2diffLim]= np.nan
-        CCNC_data.loc[abs(CCNC_data['T3 Set'] - CCNC_data['T3 Read']) > T3diffLim]= np.nan
-        CCNC_data.loc[abs(CCNC_data['Nafion Set'] - CCNC_data['T Nafion']) > NafionTdiffLim]= np.nan
-        CCNC_data.loc[abs(CCNC_data['OPC Set'] - CCNC_data['T OPC']) > 1]= np.nan 
+        CCNC_data.loc[abs(CCNC_data['T1 Set'] -                        \
+                          CCNC_data['T1 Read']) > T1diffLim] = np.nan
+        CCNC_data.loc[abs(CCNC_data['T2 Set'] -                        \
+                          CCNC_data['T2 Read']) > T2diffLim]= np.nan
+        CCNC_data.loc[abs(CCNC_data['T3 Set'] -                        \
+                          CCNC_data['T3 Read']) > T3diffLim]= np.nan
+        CCNC_data.loc[abs(CCNC_data['Nafion Set'] -                    \
+                          CCNC_data['T Nafion']) > NafionTdiffLim]= np.nan
+        CCNC_data.loc[abs(CCNC_data['OPC Set'] -                       \
+                          CCNC_data['T OPC']) > 1]= np.nan 
         
     return CCNC_data#, ReviewData)
 
 
     
 def uwy_filter(uwy_merge_data,
-               uwy_path='c:/Dropbox/RuhiFiles/Research/Projects/2016_V02_CAPRICORN/Data_Raw/Underway/'):
-    import numpy as np
-    import pandas as pd
+               uwy_path):
+    
+    assert isinstance(uwy_path, str), "Please define data path as string"
     if 'mask' not in uwy_merge_data.columns:
         import RVI_Underway
-        RVI_Underway.create_uwy_masks(uwy_path,apply_mask_to_create_filt_dataset=False)
+        RVI_Underway.create_uwy_masks(uwy_path,
+                                      apply_mask_to_create_filt_dataset=False)
         
     uwy_merge_data.loc[pd.isnull(uwy_merge_data['mask'])] = np.nan
 
@@ -416,18 +458,23 @@ def uwy_filter(uwy_merge_data,
 
 def flow_cal(data, measured_flows_df, set_flow_rate, polydeg=2):
     ''' Calibrates CPC_data for measured flow rates.
-    data - dataframe of raw CPC data
-    measured_flows_df - a dataframe of the times and measured flow rates used for calibration. See CAPRICORN.py for an example
-    set_flow_rate - the flow rate that the instrument SHOULD be at.
-    polydeg - the degree of the polynomial to fit to the measured data and correct with.
+    Parameters:
+     - data - dataframe of raw CPC data
+     - measured_flows_df - a dataframe of the times and measured flow rates 
+         used for calibration. See CAPRICORN.py for an example
+     - set_flow_rate - the flow rate that the instrument SHOULD be at.
+     - polydeg - the degree of the polynomial to fit to the measured data and 
+         correct with.
     '''
     import numpy as np
     import pandas as pd
     # Convert dates to seconds since 1 Jan 2000
-    x = (measured_flows_df.index - pd.to_datetime('2000-01-01 00:00:00')).total_seconds()
+    x = (measured_flows_df.index - \
+         pd.to_datetime('2000-01-01 00:00:00')).total_seconds()
     y = measured_flows_df['flow rate']
     p = np.poly1d(np.polyfit(x,y,deg=polydeg))
-    x_data = (data.index - pd.to_datetime('2000-01-01 00:00:00')).total_seconds()
+    x_data = (data.index - \
+              pd.to_datetime('2000-01-01 00:00:00')).total_seconds()
 
     data['CCN Number Conc'] = data['CCN Number Conc']/set_flow_rate*p(x_data)
     #plt.plot(x,y,'.',xp,p(xp),'--')
@@ -459,7 +506,8 @@ def LoadAndProcess(CCN_path,
     os.chdir(CCN_path)
     
     import glob
-    # Check if any h5 file has been produced yet (ie. the initial processing has occurred)
+    # Check if any h5 file has been produced yet 
+    # (ie. the initial processing has occurred)
     if not glob.glob('*.h5'):
         Load_to_HDF(CCN_path,filename_base)
         
@@ -483,12 +531,14 @@ def LoadAndProcess(CCN_path,
             ccn = pd.read_hdf(filename_1sec,key=filename_base)
         elif os.path.isfile(filename_raw):
             ccn = pd.read_hdf(filename_raw,key=filename_base)
-        if os.path.isfile(filename): # Return the raw file if resampling has already been done
+        # Return the raw file if resampling has already been done
+        if os.path.isfile(filename): 
             return ccn
         else:
             NeedsTimeResampling = True
     else:
-        print("No hdf file exists with the raw data! Please run the following function before this one: CCNC.Load_to_HDF")
+        print("No hdf file exists with the raw data! Please run the following \
+              function before this one: CCNC.Load_to_HDF")
         return
         
     filt = False #Initialise
@@ -498,11 +548,13 @@ def LoadAndProcess(CCN_path,
         
         # Flow calibrations
         if (not CCN_flow_check_df == ''):
-            ccn = flow_cal(ccn,CCN_flow_check_df,CCN_flow_setpt,polydeg=CCN_flow_polyDeg)
+            ccn = flow_cal(ccn,CCN_flow_check_df,
+                           CCN_flow_setpt,polydeg=CCN_flow_polyDeg)
             filt = True
         # work through mask periods and set values to nan
         for i in range(int(len(mask_period_timestamp_list)/2)):
-            ccn.loc[(ccn.index >= mask_period_timestamp_list[2*i]) & (ccn.index < mask_period_timestamp_list[2*i+1])]= np.nan
+            ccn.loc[(ccn.index >= mask_period_timestamp_list[2*i]) & 
+                    (ccn.index < mask_period_timestamp_list[2*i+1])]= np.nan
             filt = True
         # Save to file as 1 second filtered data  
         if filt:
@@ -514,7 +566,9 @@ def LoadAndProcess(CCN_path,
     
     if NeedsTimeResampling:     
         #Resample to 5 second time base, then join with UWY dataset
-        ccn = resample_timebase(data = ccn, variable = filename_base, time_int=[timeResolution])   
+        ccn = resample_timebase(data = ccn, 
+                                variable = filename_base, 
+                                time_int=[timeResolution])   
         
         ccn = ccn_stat_filt(data = ccn, std_lim = 150, removeData=True)
         
